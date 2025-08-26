967659cc2ac7a8dd1f6f9c13234883b33c363c79
vivektripathi360 <montitripathi12345@gmail.com>
2025-08-26 18:00:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "stg" {
   depends_on = [ azurerm_resource_group.rg]
-  name                     = "stg987123987"
+  name                     = "stg981"
   resource_group_name      = azurerm_resource_group.rg[0].name
   location                 = azurerm_resource_group.rg[0].location
   account_tier             = "Standard"
